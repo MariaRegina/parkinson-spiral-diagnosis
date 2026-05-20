6753308449e77ac1620581ea5d3e14ffc38cc561
Maria Regina <santosdedeusm@gmail.com>
2026-05-19 23:46:57 -0300

    added filter and variations at dataset imagens

--- src/image-processing.py
+++ src/image-processing.py
@@ -1,9 +1,16 @@
 import cv2
 import albumentations as A
 import os
+import numpy as np
 
 transform = A.Compose([
-    A.Rotate(limit=30, p=0.5),
+    A.Affine(
+        rotate=(-45,45),
+        fill=(255,255,255),
+        border_mode=cv2.BORDER_CONSTANT,
+        interpolation=cv2.INTER_LANCZOS4,
+        p=1.0
+    )
 ])
 
 transformS = A.Compose([
@@ -11,6 +18,7 @@ transformS = A.Compose([
 ])
 
 for root, dirs, files in os.walk('../database'):
+    index = 1
     for file in files:
         caminho = os.path.join(root, file)
         imagem = cv2.imread(caminho)
@@ -19,8 +27,33 @@ for root, dirs, files in os.walk('../database'):
             print(f"Erro ao carregar: {caminho}")
             continue
 
+        lower_blue = np.array([90, 50, 50])
+        upper_blue = np.array([140, 255, 255])
+
         augmented = transform(image=imagem)["image"]
-        augmentedA = transformS(image=imagem)["image"]
-        nome = os.path.splitext(file)[0]
-        cv2.imwrite(f"{nome}_aug_0.jpg", augmented)
-        cv2.imwrite(f"{nome}_aug_1.jpg", augmentedA)
+
+        hsvOriginal = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(augmented, cv2.COLOR_BGR2HSV)
+
+        maskOriginal = cv2.inRange(hsvOriginal, lower_blue, upper_blue)
+
+        original = np.ones_like(imagem) * 255
+
+        original[maskOriginal> 0] = [255, 0, 0]
+
+        augmented = transform(image=original)["image"]
+        augmentedA = transformS(image=original)["image"]
+
+        if 'Control' in caminho:
+            print('aqui')
+            cv2.imwrite("../processed-images/Control/" + str(index) + "-rotated.jpg", augmented)
+            cv2.imwrite("../processed-images/Control/" + str(index) + "-noise.jpg", augmentedA)
+            cv2.imwrite("../processed-images/Control/" + str(index) + "-original.jpg", original)
+        else:
+            cv2.imwrite("../processed-images/Parkinson/" + str(index) + "-rotated.jpg", augmented)
+            cv2.imwrite("../processed-images/Parkinson/" + str(index) + "-noise.jpg", augmentedA)
+            cv2.imwrite("../processed-images/Parkinson/" + str(index) + "-original.jpg", original)
+
+        index += 1
+
+print('Processamento finalizado')
